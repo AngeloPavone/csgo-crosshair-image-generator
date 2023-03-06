8c4bd926017c472910dc09f9c9a918a87b9e6ef5
Angelo Pavone <pavone@gmx.us>
2023-03-06 09:08:15 -0800

    refactored a bit and added all the math

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # csgo-crosshair-image-generator
 generates a png image of a given crosshair with a transparent background 
+ 
+ ported to python from https://gist.github.com/saul/d5b1a0e2284e2bf767793880f03bd729
--- generate.py
+++ generate.py
@@ -25,54 +25,49 @@ class Crosshair:
     is_t_style = None,          # HIDWORD [14] & 8
 
 
-def decode(SHARE_CODE: str) -> list:
-    crosshair_code = SHARE_CODE[4:].replace('-','')
+    def decode(SHARE_CODE: str) -> list:
+        crosshair_code = SHARE_CODE[4:].replace('-','')
 
-    DICTIONARY = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijkmnopqrstuvwxyz23456789"
-    MATCH = re.search("^CSGO(-?[\\w]{5}){5}$", SHARE_CODE)
+        DICTIONARY = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijkmnopqrstuvwxyz23456789"
+        MATCH = re.search("^CSGO(-?[\\w]{5}){5}$", SHARE_CODE)
 
-    if not MATCH:
-        print('Not a Valid Crosshair Code!')
-        exit(1)
-
-
-    big = 0
-    for char in list(crosshair_code[::-1]):
-        big = (big * len(DICTIONARY)) + DICTIONARY.index(char)
-
-    big_bytes = list(big.to_bytes(18)[::-1])
-    print(len(big_bytes))
-
-    # TODO: add a check to verify that its unsigned like this from the cshapr code
-    '''
-    // sometimes the number isn't unsigned, add a 00 byte at the end of the array to make sure it is
-    if (all.Length == 18)
-        all = all.Concat(new byte[] {0}).ToArray();
-    return all.Reverse().ToArray();
-    '''
-
-    return big_bytes
+        if not MATCH:
+            print('Not a Valid Crosshair Code!')
+            exit(1)
 
 
-# TODO: add all the math and fix the ones that don't work
-def crosshair_info(raw_bytes=decode(SHARE_CODE)):
-    Crosshair.outline = raw_bytes[4] / 2.0,
-    Crosshair.red = raw_bytes[5],
-    Crosshair.green = raw_bytes[6],
-    Crosshair.blue = raw_bytes[7],
-    Crosshair.alpha = raw_bytes[8],
-    Crosshair.crosshair_style = ((raw_bytes[14] & 0xF) >> 1),
-    Crosshair.has_center_dot = ((raw_bytes[14] >> 4) & 1) != 0,
-    Crosshair.length = (raw_bytes[15] / 10.0),
-    Crosshair.thickness = (raw_bytes[13] / 10.0),
-    Crosshair.gap = (raw_bytes[3] / 10.0),
-    Crosshair.hasOutline = None,
-    Crosshair.split_distance = raw_bytes[9],
-    Crosshair.has_alpha = None,
-    Crosshair.inner_split_alpha = None,
-    Crosshair.outer_split_alpha = None,
-    Crosshair.split_size_ratio = None,
-    Crosshair.is_t_style = None,
+        big = 0
+        for char in list(crosshair_code[::-1]):
+            big = (big * len(DICTIONARY)) + DICTIONARY.index(char)
+
+        big_bytes = list(big.to_bytes(18)[::-1])
+
+        if len(big_bytes) == 18:
+            big_bytes.append(0x00)
+
+        print(f'RAW BYTES: {big_bytes}')
+        return big_bytes
+
+
+    # TODO: add all the math and fix the ones that don't work
+    def __init__(self, raw_bytes=decode(SHARE_CODE)) -> object:
+        self.gap               =    (raw_bytes[3] / 10.0),
+        self.outline           =    (raw_bytes[4] / 2.0),
+        self.red               =    (raw_bytes[5]),
+        self.green             =    (raw_bytes[6]),
+        self.blue              =    (raw_bytes[7]),
+        self.alpha             =    (raw_bytes[8]),
+        self.split_distance    =    (raw_bytes[9]),
+        self.hasOutline        =    (bytes[11] & 8) != 0,
+        self.thickness         =    (raw_bytes[13] / 10.0),
+        self.crosshair_style   =    (raw_bytes[14] & 0xF) >> 1,
+        self.has_center_dot    =    ((raw_bytes[14] >> 4) & 1) != 0,
+        self.has_alpha         =    ((bytes[14] >> 4) & 4) != 0,
+        self.length            =    (raw_bytes[15] / 10.0),
+        self.inner_split_alpha =    (bytes[11] >> 4) / 10.0,
+        self.outer_split_alpha =    (bytes[12] & 0xF) / 10.0,
+        self.split_size_ratio  =    (bytes[12] >> 4) / 10.0,
+        self.is_t_style        =    ((bytes[14] >> 4) & 8) != 0,
 
 
 def create_image() -> object:
@@ -85,7 +80,9 @@ def create_image() -> object:
 
 def main():
     create_image()
-    crosshair_info()
+    c = Crosshair()
+
+    print(f"")
 
 
 if __name__ == '__main__':
