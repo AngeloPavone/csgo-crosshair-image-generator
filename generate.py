77d69c94ec05b832969d373596195c3b2640af72
Angelo Pavone <pavone@gmx.us>
2023-03-06 14:51:21 -0800

    added printing for each crosshair option

--- generate.py
+++ generate.py
@@ -51,23 +51,24 @@ class Crosshair:
 
     # TODO: add all the math and fix the ones that don't work
     def __init__(self, raw_bytes=decode(SHARE_CODE)) -> object:
+        self.style              =   (raw_bytes[14] & 0xF) >> 1
+        self.is_t_style         =   (1 if ((raw_bytes[14] >> 4) & 8) != 0 else 0)
+        self.length             =   (raw_bytes[15] / 10.0)
+        self.thickness          =   (raw_bytes[13] / 10.0)
         self.gap                =   (raw_bytes[3] / 10.0)
+        self.has_center_dot     =   (1 if ((raw_bytes[14] >> 4) & 1) != 0 else 0)
+        self.has_alpha          =   (1 if ((raw_bytes[14] >> 4) & 4) != 0 else 0)
+        self.alpha              =   (raw_bytes[8])
         self.outline            =   (raw_bytes[4] / 2.0)
         self.red                =   (raw_bytes[5])
         self.green              =   (raw_bytes[6])
         self.blue               =   (raw_bytes[7])
-        self.alpha              =   (raw_bytes[8])
-        self.split_distance     =   (raw_bytes[9])
         self.has_outline        =   (1 if (raw_bytes[11] & 8) != 0 else 0)
-        self.thickness          =   (raw_bytes[13] / 10.0)
-        self.style              =   (raw_bytes[14] & 0xF) >> 1
-        self.has_center_dot     =   (1 if ((raw_bytes[14] >> 4) & 1) != 0 else 0)
-        self.has_alpha          =   (1 if ((raw_bytes[14] >> 4) & 4) != 0 else 0)
-        self.length             =   (raw_bytes[15] / 10.0)
         self.inner_split_alpha  =   (raw_bytes[11] >> 4) / 10.0
         self.outer_split_alpha  =   (raw_bytes[12] & 0xF) / 10.0
+        self.split_distance     =   (raw_bytes[9])
+
         self.split_size_ratio   =   (raw_bytes[12] >> 4) / 10.0
-        self.is_t_style         =   (1 if ((raw_bytes[14] >> 4) & 8) != 0 else 0)
 
 
 def create_image() -> object:
@@ -81,7 +82,25 @@ def create_image() -> object:
 def main():
     create_image()
     c = Crosshair()
-    print(f"cl_crosshairstyle {c.style}, cl_crosshair_t {c.is_t_style}, cl_crosshairsize {c.length}, cl_Crosshairthickness {c.thickness}, cl_crosshairgap {c.gap}, cl_crosshairdot {c.has_center_dot}, cl_crosshairusealpha {c.has_alpha}, cl_crosshairalpha {c.alpha}, cl_crosshair_drawoutline {c.has_outline}")
+    print(f'cl_crosshairstyle {c.style};'
+          f' cl_crosshair_t {c.is_t_style};'
+          f' cl_crosshairsize {c.length};'
+          f' cl_crosshairthickness {c.thickness};'
+          f' cl_crosshairgap {c.gap};'
+          f' cl_crosshairdot {c.has_center_dot};'
+          f' cl_crosshairusealpha {c.has_alpha};'
+          f' cl_crosshairalpha {c.alpha};'
+          f' cl_crosshair_drawoutline {c.has_outline};'
+          f' cl_crosshaircolor 5;'
+          f' cl_crosshaircolor_r {c.red};'
+          f' cl_crosshaircolor_g {c.green};'
+          f' cl_crosshaircolor_b {c.blue};'
+          f' cl_crosshair_dynamic_splitdist {c.split_distance};'
+          f' cl_crosshair_outlinethickness {c.outline};'
+          f' cl_crosshair_dynamic_splitalpha_innermod {c.inner_split_alpha};'
+          f' cl_crosshair_dynamic_splitalpha_outermod {c.outer_split_alpha};'
+          f' cl_crosshair_dynamic_maxdist_splitratio {c.split_size_ratio}'
+        )
 
 
 
