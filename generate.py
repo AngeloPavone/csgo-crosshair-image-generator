791151076c52add8a8f187d9b0325df3b26a792f
Angelo Pavone <pavone@gmx.us>
2023-03-09 10:14:31 -0800

    finally fixed all the math

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ import re
 from math import log
 
 
-SHARE_CODE = 'CSGO-2phoc-Nn9Pa-qrMFN-xtXZP-O3qRP'
+SHARE_CODE = 'CSGO-Cn37R-YE7vo-pLCAL-aURmZ-z6zkG'
 
 SCALE = 1
 WIDTH = 200
@@ -14,12 +14,18 @@ CENTER_Y = HEIGHT // 2
 
 class Crosshair:
     style               =   None
+        # style 0 => Default
+        # style 1 => Default static
+        # style 2 => Classic
+        # style 3 => Classic dynamic
+        # style 4 => Classic static
     has_center_dot      =   None
     size                =   None
     thickness           =   None
     gap                 =   None
+    fixed_gap           =   None
     has_outline         =   None
-    outline             =   None
+    outline_thickness   =   None
     red                 =   None
     green               =   None
     blue                =   None
@@ -30,9 +36,10 @@ class Crosshair:
     outer_split_alpha   =   None
     split_size_ratio    =   None
     is_t_style          =   None
+    use_weapon_gap      =   None
 
 
-    def decode(SHARE_CODE: str) -> list:
+    def code_to_bytes(SHARE_CODE: str) -> list:
 
         crosshair_code = SHARE_CODE[4:].replace('-','')
 
@@ -44,41 +51,46 @@ class Crosshair:
             exit(1)
 
         big = 0
-        for char in list(crosshair_code[::-1]):
+        for char in list(reversed(crosshair_code)):
             big = (big * len(DICTIONARY)) + DICTIONARY.index(char)
 
         def bytes_needed(n):
             return 1 if n == 0 else int(log(n, 256)) + 1
 
-        big_size = bytes_needed(big)
-        big_bytes = list(big.to_bytes(big_size, 'little'))
+        bytes_required = bytes_needed(big)
+        bytes = list(big.to_bytes(bytes_required, 'little'))
 
-        if len(big_bytes) == 18:
-            big_bytes.append(0x00)
+        if len(bytes) == 18:
+            bytes.append(0x00)
 
-        print(f'\n {big_bytes}\n')
-        return big_bytes
+        print(f'\n {bytes}\n')
+        return list(reversed(bytes))
 
 
-    def __init__(self, raw_bytes=decode(SHARE_CODE)) -> None:
-        self.style              =   (raw_bytes[14] & 0xF) >> 1
-        self.is_t_style         =   (1 if ((raw_bytes[14] >> 4) & 8) != 0 else 0)
-        self.size               =   (raw_bytes[15] / 10.0)
-        self.thickness          =   (raw_bytes[13] / 10.0)
-        # self.use_weapon_gap     =   (1 if ((raw_bytes[13] >> 4) & 2) != 0 else 0)
-        self.gap                =   (raw_bytes[3] if raw_bytes[3] < 128 else raw_bytes[3] - 256) / 10.0
-        self.has_center_dot     =   (1 if ((raw_bytes[14] >> 4) & 1) != 0 else 0)
-        self.has_alpha          =   (1 if ((raw_bytes[14] >> 4) & 4) != 0 else 0)
-        self.alpha              =   (raw_bytes[8])
-        self.outline            =   (raw_bytes[4] / 2.0)
-        self.red                =   (raw_bytes[5])
-        self.green              =   (raw_bytes[6])
-        self.blue               =   (raw_bytes[7])
-        self.has_outline        =   (1 if (raw_bytes[11] & 8) != 0 else 0)
-        self.inner_split_alpha  =   (raw_bytes[11] >> 4) / 10.0
-        self.outer_split_alpha  =   (raw_bytes[12] & 0xF) / 10.0
-        self.split_distance     =   (raw_bytes[9])
-        self.split_size_ratio   =   (raw_bytes[12] >> 4) / 10.0
+    def uint8toint8(input: int) -> int:
+        return input if input < 128 else input - 256
+
+    def __init__(self,  bytes=code_to_bytes(SHARE_CODE)) -> None:
+        self.gap                =   (Crosshair.uint8toint8(bytes[3]) / 10.0)
+        self.outline_thickness  =   (bytes[4] / 2)
+        self.red                =   (bytes[5])
+        self.green              =   (bytes[6])
+        self.blue               =   (bytes[7])
+        self.alpha              =   (bytes[8])
+        self.split_distance     =   (bytes[9])
+        self.fixed_gap          =   (Crosshair.uint8toint8((bytes[10]) / 10.0))
+        self.color              =   (bytes[11] & 7)
+        self.has_outline        =   (1 if (bytes[11] & 8) != 0 else 0)
+        self.inner_split_alpha  =   (bytes[11] >> 4) / 10.0
+        self.outer_split_alpha  =   (bytes[12] & 0xF) / 10.0
+        self.split_size_ratio   =   (bytes[12] >> 4) / 10.0
+        self.thickness          =   (bytes[13] / 10.0)
+        self.has_center_dot     =   (1 if ((bytes[14] >> 4) & 1) != 0 else 0)
+        self.use_weapon_gap     =   (1 if ((bytes[14] >> 4) & 2) != 0 else 0)
+        self.has_alpha          =   (1 if ((bytes[14] >> 4) & 4) != 0 else 0)
+        self.is_t_style         =   (1 if ((bytes[14] >> 4) & 8) != 0 else 0)
+        self.style              =   (bytes[14] & 0xF) >> 1
+        self.size               =   (bytes[15] / 10.0)
 
 
 def create_image() -> None:
@@ -117,24 +129,26 @@ def print_crosshair() -> None:
     c = Crosshair()
     print(f'\n CODE: {SHARE_CODE}\n')
     print(
-            f' cl_crosshairstyle {c.style};\n'
-            f' cl_crosshair_t {c.is_t_style};\n'
-            f' cl_crosshairsize {c.size};\n'
-            f' cl_crosshairthickness {c.thickness};\n'
             f' cl_crosshairgap {c.gap};\n'
-            f' cl_crosshairdot {c.has_center_dot};\n\n'
-            f' cl_crosshairusealpha {c.has_alpha};\n'
-            f' cl_crosshairalpha {c.alpha};\n'
-            f' cl_crosshair_drawoutline {c.has_outline};\n'
-            f' cl_crosshaircolor 5;\n'
+            f' cl_crosshair_outlinethickness {c.outline_thickness};\n'
             f' cl_crosshaircolor_r {c.red};\n'
             f' cl_crosshaircolor_g {c.green};\n'
-            f' cl_crosshaircolor_b {c.blue};\n\n'
+            f' cl_crosshaircolor_b {c.blue};\n'
+            f' cl_crosshairalpha {c.alpha};\n\n'
             f' cl_crosshair_dynamic_splitdist {c.split_distance};\n'
-            f' cl_crosshair_outlinethickness {c.outline};\n'
+            f' cl_fixedcrosshairgap {c.fixed_gap}\n'
+            f' cl_crosshaircolor {c.color};\n'
+            f' cl_crosshair_drawoutline {c.has_outline};\n'
             f' cl_crosshair_dynamic_splitalpha_innermod {c.inner_split_alpha};\n'
-            f' cl_crosshair_dynamic_splitalpha_outermod {c.outer_split_alpha};\n'
+            f' cl_crosshair_dynamic_splitalpha_outermod {c.outer_split_alpha};\n\n'
             f' cl_crosshair_dynamic_maxdist_splitratio {c.split_size_ratio}\n'
+            f' cl_crosshairthickness {c.thickness};\n'
+            f' cl_crosshairdot {c.has_center_dot};\n'
+            f' cl_crosshairgap_useweaponvalue {c.use_weapon_gap};\n'
+            f' cl_crosshairusealpha {c.has_alpha};\n'
+            f' cl_crosshair_t {c.is_t_style};\n'
+            f' cl_crosshairstyle {c.style};\n'
+            f' cl_crosshairsize {c.size};\n'
         )
 
 
